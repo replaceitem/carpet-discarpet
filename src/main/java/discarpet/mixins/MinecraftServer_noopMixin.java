1d0cd825585803badaddc0361a883bf010189ee3
replaceitem <40722305+replaceitem@users.noreply.github.com>
2020-09-28 18:20:49 +0200

    Fixed only one extension loading when multiple are present

--- src/main/java/discarpet/mixins/MinecraftServer_noopMixin.java
+++ src/main/java/discarpet/mixins/MinecraftServer_noopMixin.java
@@ -2,20 +2,17 @@ package discarpet.mixins;
 
 import discarpet.Discarpet;
 import net.minecraft.server.Main;
+import net.minecraft.util.crash.CrashReport;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(Main.class)
+@Mixin(CrashReport.class)
 public class MinecraftServer_noopMixin
 {
-    // this is here just to load the ExampleExtension class, otherwise noone would load it / need it
-    // if you have already you own mixins that use your extension class in any shape or form
-    // you don't need this one
-    // You need this one to run a server properly
-    @Inject(method = "main", at = @At("HEAD"))
-    private static void onServerStarted(String[] args, CallbackInfo ci) {
+    @Inject(method = "initCrashReport", at = @At("HEAD"))
+    private static void onServerStarted(CallbackInfo ci) {
         Discarpet.noop();
     }
 }
