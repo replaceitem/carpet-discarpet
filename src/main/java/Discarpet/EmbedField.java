64c523d3aee29751d28f82e8e998856ea339e577
replaceitem <40722305+replaceitem@users.noreply.github.com>
2020-10-12 16:19:02 +0200

    Moved EmbedField out of classes directory

---
+++ src/main/java/Discarpet/EmbedField.java
@@ -0,0 +1,12 @@
+package Discarpet;
+
+public class EmbedField {
+	public boolean inline;
+	public String name;
+	public String value;
+	public EmbedField(boolean inline, String name, String value) {
+		this.inline = inline;
+		this.name = name;
+		this.value = value;
+	}
+}
