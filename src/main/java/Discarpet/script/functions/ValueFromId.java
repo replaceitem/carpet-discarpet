fd11994305361232633c33b68da3d97a1be92ce9
replaceitem <40722305+replaceitem@users.noreply.github.com>
2020-12-29 13:09:20 +0100

    dc_emoji_from_id now also returns emojis with a specified name

--- src/main/java/Discarpet/script/functions/ValueFromId.java
+++ src/main/java/Discarpet/script/functions/ValueFromId.java
@@ -5,12 +5,14 @@ import Discarpet.script.values.ChannelValue;
 import Discarpet.script.values.EmojiValue;
 import Discarpet.script.values.ServerValue;
 import carpet.script.Expression;
+import carpet.script.value.ListValue;
 import carpet.script.value.Value;
 import org.javacord.api.entity.channel.Channel;
 import org.javacord.api.entity.channel.TextChannel;
 import org.javacord.api.entity.emoji.KnownCustomEmoji;
 import org.javacord.api.entity.server.Server;
 
+import java.util.Collection;
 import java.util.Optional;
 
 import static Discarpet.Discarpet.*;
@@ -54,11 +56,11 @@ public class ValueFromId {
 
             Optional<KnownCustomEmoji> optionalEmoji = (((ServerValue) serverValue).server.getCustomEmojiById(emojiValue.getString()));
 
-            if(optionalEmoji.isPresent()) {
-                return (cc, tt) -> new EmojiValue(optionalEmoji.get());
-            } else {
-                return NULL;
-            }
+            if(optionalEmoji.isPresent()) return (cc, tt) -> new EmojiValue(optionalEmoji.get());
+
+            Collection<KnownCustomEmoji> emojis = (((ServerValue) serverValue).server.getCustomEmojisByName(emojiValue.getString()));
+
+            return (cc,tt) -> ListValue.wrap(emojis.stream().map(EmojiValue::new));
         });
     }
 }
