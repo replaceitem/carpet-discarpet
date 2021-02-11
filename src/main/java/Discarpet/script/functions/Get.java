3a429212a69259a2d4ebcbf396fd5261087de435
replaceitem <40722305+replaceitem@users.noreply.github.com>
2021-02-11 18:13:26 +0100

    Added dc_get_bot_user to get the bot itself

--- src/main/java/Discarpet/script/functions/Get.java
+++ src/main/java/Discarpet/script/functions/Get.java
@@ -1,12 +1,13 @@
 package Discarpet.script.functions;
 
+import Discarpet.Bot;
 import Discarpet.script.values.ServerValue;
 import Discarpet.script.values.UserValue;
 
 import carpet.script.Expression;
 import carpet.script.value.*;
 
-import static Discarpet.Discarpet.scarpetException;
+import static Discarpet.Discarpet.*;
 
 public class Get {
     public static void apply(Expression expr) {
@@ -16,6 +17,12 @@ public class Get {
             if(!(user instanceof UserValue)) scarpetException("dc_get_display_name","user",0);
             if(!(server instanceof ServerValue)) scarpetException("dc_get_display_name","server",1);
             return (cc, tt) -> StringValue.of(((ServerValue) server).server.getDisplayName(((UserValue) user).user));
+        });
+
+        expr.addLazyFunction("dc_get_bot_user", 0, (c, t, lv) -> {
+            Bot b = getBotInContext(c);
+            if(b==null) scarpetNoBotException("dc_get_bot_user");
+            return (cc, tt) -> new UserValue(b.getApi().getYourself());
         });
     }
 }
