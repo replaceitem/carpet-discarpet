9ee8406b1c1d6e6dabf83509bec2fb2fa3e4c6fa
replaceitem <40722305+replaceitem@users.noreply.github.com>
2022-01-02 22:54:26 +0100

    role value by id

--- src/main/java/Discarpet/script/functions/ValueFromId.java
+++ src/main/java/Discarpet/script/functions/ValueFromId.java
@@ -3,18 +3,12 @@ package Discarpet.script.functions;
 import Discarpet.Bot;
 import Discarpet.script.values.ChannelValue;
 import Discarpet.script.values.EmojiValue;
+import Discarpet.script.values.RoleValue;
 import Discarpet.script.values.ServerValue;
 import carpet.script.Context;
 import carpet.script.annotation.ScarpetFunction;
-import carpet.script.value.ListValue;
 import carpet.script.value.Value;
-import org.javacord.api.entity.channel.Channel;
-import org.javacord.api.entity.channel.TextChannel;
-import org.javacord.api.entity.emoji.KnownCustomEmoji;
-import org.javacord.api.entity.server.Server;
 
-import java.util.Collection;
-import java.util.Optional;
 
 import static Discarpet.Discarpet.*;
 
@@ -23,38 +17,27 @@ public class ValueFromId {
 	public Value dc_channel_from_id(Context ctx, String channelId) {
 		Bot bot = getBotInContext(ctx);
         if (bot == null) scarpetNoBotException("dc_channel_from_id");
-
-        Optional<Channel> optionalChannel = bot.api.getChannelById(channelId);
-        if (optionalChannel.isPresent() && optionalChannel.get() instanceof TextChannel) {
-            return new ChannelValue(optionalChannel.get());
-        } else {
-            return Value.NULL;
-        }
+        return ChannelValue.of(bot.api.getChannelById(channelId));
 	}
 	
 	@ScarpetFunction
 	public Value dc_server_from_id(Context ctx, String serverId) {
 		Bot bot = getBotInContext(ctx);
         if (bot == null) scarpetNoBotException("dc_server_from_id");
-
-        Optional<Server> optionalServer = bot.api.getServerById(serverId);
-        if(optionalServer.isPresent()) {
-            return new ServerValue(optionalServer.get());
-        } else {
-            return Value.NULL;
-        }
+        return ServerValue.of(bot.api.getServerById(serverId));
 	}
 	
 	@ScarpetFunction
-	public Value dc_emoji_from_id(Context ctx, Server server, String emojiId) {
-		Bot b = getBotInContext(ctx);
-        if (b == null) scarpetNoBotException("dc_emoji_from_id");
-        Optional<KnownCustomEmoji> optionalEmoji = server.getCustomEmojiById(emojiId);
-
-        if(optionalEmoji.isPresent()) return new EmojiValue(optionalEmoji.get());
-
-        Collection<KnownCustomEmoji> emojis = server.getCustomEmojisByName(emojiId);
-
-        return ListValue.wrap(emojis.stream().map(EmojiValue::new));
+	public Value dc_emoji_from_id(Context ctx, String emojiId) {
+		Bot bot = getBotInContext(ctx);
+        if (bot == null) scarpetNoBotException("dc_emoji_from_id");
+        return EmojiValue.of(bot.api.getCustomEmojiById(emojiId));
 	}
+
+    @ScarpetFunction
+    public Value dc_role_from_id(Context ctx, String roleId) {
+        Bot bot = getBotInContext(ctx);
+        if (bot == null) scarpetNoBotException("dc_emoji_from_id");
+        return RoleValue.of(bot.api.getRoleById(roleId));
+    }
 }
