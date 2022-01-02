11289c60652be104d436d0288c7a496d18fba94a
replaceitem <40722305+replaceitem@users.noreply.github.com>
2022-01-02 23:13:48 +0100

    Add and remove roles

--- src/main/java/Discarpet/script/functions/Users.java
+++ src/main/java/Discarpet/script/functions/Users.java
@@ -2,6 +2,7 @@ package Discarpet.script.functions;
 
 import Discarpet.script.util.ValueUtil;
 import carpet.script.annotation.ScarpetFunction;
+import org.javacord.api.entity.permission.Role;
 import org.javacord.api.entity.server.Server;
 import org.javacord.api.entity.user.User;
 
@@ -14,5 +15,15 @@ public class Users {
     @ScarpetFunction
     public boolean dc_set_nickname(User user, Server server, String name) {
         return ValueUtil.awaitFutureBoolean(user.updateNickname(server, name),"Error updating nickname");
+    }
+
+    @ScarpetFunction
+    public boolean dc_add_role(User user, Role role, String reason) {
+        return ValueUtil.awaitFutureBoolean(user.addRole(role, reason),"Could not add role to user");
+    }
+
+    @ScarpetFunction
+    public boolean dc_remove_role(User user, Role role, String reason) {
+        return ValueUtil.awaitFutureBoolean(user.removeRole(role, reason),"Could not add role to user");
     }
 }
