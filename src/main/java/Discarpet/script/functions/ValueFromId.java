259f8858c4efb58f293cc756586da56716f1432b
replaceitem <40722305+replaceitem@users.noreply.github.com>
2022-03-12 13:34:38 +0100

    Added several value from id functions

--- src/main/java/Discarpet/script/functions/ValueFromId.java
+++ src/main/java/Discarpet/script/functions/ValueFromId.java
@@ -5,35 +5,70 @@ import Discarpet.script.values.ChannelValue;
 import Discarpet.script.values.EmojiValue;
 import Discarpet.script.values.RoleValue;
 import Discarpet.script.values.ServerValue;
+import Discarpet.script.values.UserValue;
 import carpet.script.Context;
 import carpet.script.annotation.ScarpetFunction;
+import carpet.script.exception.InternalExpressionException;
 import carpet.script.value.Value;
+import org.javacord.api.entity.channel.Channel;
+import org.javacord.api.entity.channel.TextChannel;
+import org.javacord.api.entity.emoji.Emoji;
+import org.javacord.api.entity.message.Message;
+import org.javacord.api.entity.permission.Role;
+import org.javacord.api.entity.server.Server;
+import org.javacord.api.entity.user.User;
+import org.javacord.api.entity.webhook.Webhook;
 
 
 import static Discarpet.Discarpet.*;
 
 public class ValueFromId {
 	@ScarpetFunction
-	public Value dc_channel_from_id(Context ctx, String channelId) {
+	public Channel dc_channel_from_id(Context ctx, String channelId) {
 		Bot bot = getBotInContext(ctx,"dc_channel_from_id");
-        return ChannelValue.of(bot.api.getChannelById(channelId));
+        return bot.api.getChannelById(channelId).orElse(null);
 	}
 	
 	@ScarpetFunction
-	public Value dc_server_from_id(Context ctx, String serverId) {
+	public Server dc_server_from_id(Context ctx, String serverId) {
 		Bot bot = getBotInContext(ctx,"dc_server_from_id");
-        return ServerValue.of(bot.api.getServerById(serverId));
+        return bot.api.getServerById(serverId).orElse(null);
 	}
 	
 	@ScarpetFunction
-	public Value dc_emoji_from_id(Context ctx, String emojiId) {
+	public Emoji dc_emoji_from_id(Context ctx, String emojiId) {
 		Bot bot = getBotInContext(ctx,"dc_emoji_from_id");
-        return EmojiValue.of(bot.api.getCustomEmojiById(emojiId));
+        return bot.api.getCustomEmojiById(emojiId).orElse(null);
 	}
 
     @ScarpetFunction
-    public Value dc_role_from_id(Context ctx, String roleId) {
+    public Role dc_role_from_id(Context ctx, String roleId) {
         Bot bot = getBotInContext(ctx,"dc_role_from_id");
-        return RoleValue.of(bot.api.getRoleById(roleId));
+        return bot.api.getRoleById(roleId).orElse(null);
     }
+
+	@ScarpetFunction
+	public User dc_user_from_id(Context ctx, String userId) {
+		Bot bot = getBotInContext(ctx,"dc_user_from_id");
+		return bot.api.getUserById(userId).join();
+	}
+	
+	@ScarpetFunction
+	public Message dc_message_from_id(Context ctx, String messageId, Channel channel) {
+		Bot bot = getBotInContext(ctx,"dc_user_from_id");
+		if(!(channel instanceof TextChannel textChannel)) return null;
+		return bot.api.getMessageById(messageId, textChannel).join();
+	}
+
+	@ScarpetFunction
+	public Webhook dc_webhook_from_id(Context ctx, String webhookId, String token) {
+		Bot bot = getBotInContext(ctx,"dc_user_from_id");
+		return bot.api.getIncomingWebhookByIdAndToken(webhookId, token).join();
+	}
+
+	@ScarpetFunction
+	public Webhook dc_webhook_from_url(Context ctx, String webhookUrl) {
+		Bot bot = getBotInContext(ctx,"dc_user_from_id");
+		return bot.api.getIncomingWebhookByUrl(webhookUrl).join();
+	}
 }
