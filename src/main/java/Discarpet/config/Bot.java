06e6c0802942456bf44481aa8bc294739ae04a62
replaceitem <40722305+replaceitem@users.noreply.github.com>
2022-04-13 21:45:09 +0200

    Made discord events run on the main game thread

--- src/main/java/Discarpet/config/Bot.java
+++ src/main/java/Discarpet/config/Bot.java
@@ -1,16 +1,21 @@
 package Discarpet.config;
 
+import Discarpet.script.events.DiscarpetEventsListener;
 import Discarpet.script.events.DiscordEvents;
+import carpet.CarpetServer;
+import net.minecraft.server.ServerTask;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.text.LiteralText;
 import net.minecraft.util.Formatting;
 import org.javacord.api.DiscordApi;
 import org.javacord.api.DiscordApiBuilder;
 import org.javacord.api.entity.intent.Intent;
+import org.javacord.api.entity.message.Message;
 import org.javacord.api.entity.message.Reaction;
 import org.javacord.api.interaction.ButtonInteraction;
 import org.javacord.api.interaction.MessageComponentInteraction;
 import org.javacord.api.interaction.SelectMenuInteraction;
+import org.javacord.api.interaction.SlashCommandInteraction;
 
 import java.util.HashSet;
 import java.util.Optional;
@@ -47,42 +52,9 @@ public class Bot {
 			}
 			if(source != null) source.sendFeedback(new LiteralText(msg),false);
 			LOGGER.info(msg);
+			
 			api.getListeners().keySet().forEach(globallyAttachableListener -> api.removeListener(globallyAttachableListener));
-			api.addMessageCreateListener(event -> {
-				DiscordEvents.DISCORD_MESSAGE.onDiscordMessage(this,event.getMessage());
-			});
-
-			api.addReactionAddListener(event -> {
-				Optional<Reaction> optionalReaction = event.getReaction();
-				if(optionalReaction.isPresent()) {
-					Reaction reaction = optionalReaction.get();
-					event.requestUser().thenAccept(user -> {
-						DiscordEvents.DISCORD_REACTION.onDiscordReaction(this,reaction,user,true);
-					});
-				}
-
-			});
-			api.addReactionRemoveListener(event -> {
-				Optional<Reaction> optionalReaction = event.getReaction();
-				if(optionalReaction.isPresent()) {
-					Reaction reaction = optionalReaction.get();
-					event.requestUser().thenAccept(user -> {
-						DiscordEvents.DISCORD_REACTION.onDiscordReaction(this,reaction,user,false);
-					});
-				}
-			});
-			api.addSlashCommandCreateListener(event -> {
-				DiscordEvents.DISCORD_SLASH_COMMAND.onDiscordSlashCommand(this,event.getSlashCommandInteraction());
-			});
-			api.addMessageComponentCreateListener(event -> {
-				MessageComponentInteraction messageComponentInteraction = event.getMessageComponentInteraction();
-
-				Optional<ButtonInteraction> buttonInteraction = messageComponentInteraction.asButtonInteraction();
-				buttonInteraction.ifPresent(interaction -> DiscordEvents.DISCORD_BUTTON.onDiscordButton(this, interaction));
-
-				Optional<SelectMenuInteraction> selectMenuInteraction = messageComponentInteraction.asSelectMenuInteraction();
-				selectMenuInteraction.ifPresent(interaction -> DiscordEvents.DISCORD_SELECT_MENU.onDiscordSelectMenu(this, interaction));
-			});
+			api.addListener(new DiscarpetEventsListener(this));
 		} catch (CompletionException | InterruptedException | ExecutionException ce) {
 			String error = "Could not login bot " + id;
 			LOGGER.warn(error,ce);
--- src/main/java/Discarpet/script/events/DiscordEvents.java
+++ src/main/java/Discarpet/script/events/DiscordEvents.java
@@ -28,6 +28,15 @@ import java.util.function.Supplier;
 
 
 public class DiscordEvents extends Event {
+    
+    private static final Supplier<ServerCommandSource> DEFAULT_SOURCE_SUPPLIER = () -> {
+        try {
+            return CarpetServer.minecraft_server.getCommandSource().withWorld(CarpetServer.minecraft_server.getWorld(World.OVERWORLD));
+        } catch (NullPointerException npe) {
+            return null;
+        }
+    };
+    
     public static void noop() {} //to load events before scripts do
 
     public DiscordEvents(String name, int reqArgs, boolean isGlobalOnly) {
@@ -38,17 +47,10 @@ public class DiscordEvents extends Event {
 
     public static DiscordEvents DISCORD_MESSAGE = new DiscordEvents("discord_message", 1, false) {
         public void onDiscordMessage(Bot bot, Message message) {
-            if(bot == null) return;
-            if(CarpetServer.minecraft_server != null && !CarpetServer.minecraft_server.isRunning()) return; //prevent errors when message comes while stopping
-            callHandlerInBotApps(bot,() -> {
-                return Arrays.asList(new MessageValue(message));
-            }, () -> {
-                try {
-                    return CarpetServer.minecraft_server.getCommandSource().withWorld(CarpetServer.minecraft_server.getWorld(World.OVERWORLD));
-                } catch (NullPointerException npe) {
-                    return null;
-                }
-            });
+            callHandlerInBotApps(bot,
+                    () -> List.of(new MessageValue(message)),
+                    DEFAULT_SOURCE_SUPPLIER
+            );
         }
     };
 
@@ -56,17 +58,10 @@ public class DiscordEvents extends Event {
 
     public static DiscordEvents DISCORD_REACTION = new DiscordEvents("discord_reaction", 3, false) {
         public void onDiscordReaction(Bot bot, Reaction reaction, User user, boolean added) {
-            if(bot == null) return;
-            if(CarpetServer.minecraft_server != null && !CarpetServer.minecraft_server.isRunning()) return; //prevent errors when message comes while stopping
-            callHandlerInBotApps(bot,() -> {
-                return Arrays.asList(new ReactionValue(reaction),new UserValue(user), BooleanValue.of(added));
-            }, () -> {
-                try {
-                    return CarpetServer.minecraft_server.getCommandSource().withWorld(CarpetServer.minecraft_server.getWorld(World.OVERWORLD));
-                } catch (NullPointerException npe) {
-                    return null;
-                }
-            });
+            callHandlerInBotApps(bot,
+                    () -> List.of(new ReactionValue(reaction),new UserValue(user), BooleanValue.of(added)), 
+                    DEFAULT_SOURCE_SUPPLIER
+            );
         }
     };
 
@@ -75,17 +70,10 @@ public class DiscordEvents extends Event {
 
     public static DiscordEvents DISCORD_SLASH_COMMAND = new DiscordEvents("discord_slash_command", 1, false) {
         public void onDiscordSlashCommand(Bot bot, SlashCommandInteraction slashCommandInteraction) {
-            if(bot == null) return;
-            if(CarpetServer.minecraft_server != null && !CarpetServer.minecraft_server.isRunning()) return; //prevent errors when message comes while stopping
-            callHandlerInBotApps(bot,() -> {
-                return Arrays.asList(new SlashCommandInteractionValue(slashCommandInteraction));
-            }, () -> {
-                try {
-                    return CarpetServer.minecraft_server.getCommandSource().withWorld(CarpetServer.minecraft_server.getWorld(World.OVERWORLD));
-                } catch (NullPointerException npe) {
-                    return null;
-                }
-            });
+            callHandlerInBotApps(bot,
+                    () -> List.of(new SlashCommandInteractionValue(slashCommandInteraction)), 
+                    DEFAULT_SOURCE_SUPPLIER
+            );
         }
     };
 
@@ -94,17 +82,10 @@ public class DiscordEvents extends Event {
 
     public static DiscordEvents DISCORD_BUTTON = new DiscordEvents("discord_button", 1, false) {
         public void onDiscordButton(Bot bot, ButtonInteraction buttonInteraction) {
-            if(bot == null) return;
-            if(CarpetServer.minecraft_server != null && !CarpetServer.minecraft_server.isRunning()) return; //prevent errors when message comes while stopping
-            callHandlerInBotApps(bot,() -> {
-                return Arrays.asList(new ButtonInteractionValue(buttonInteraction));
-            }, () -> {
-                try {
-                    return CarpetServer.minecraft_server.getCommandSource().withWorld(CarpetServer.minecraft_server.getWorld(World.OVERWORLD));
-                } catch (NullPointerException npe) {
-                    return null;
-                }
-            });
+            callHandlerInBotApps(bot,
+                    () -> List.of(new ButtonInteractionValue(buttonInteraction)),
+                    DEFAULT_SOURCE_SUPPLIER
+            );
         }
     };
 
@@ -113,21 +94,15 @@ public class DiscordEvents extends Event {
 
     public static DiscordEvents DISCORD_SELECT_MENU = new DiscordEvents("discord_select_menu", 1, false) {
         public void onDiscordSelectMenu(Bot bot, SelectMenuInteraction selectMenuInteraction) {
-            if(bot == null) return;
-            if(CarpetServer.minecraft_server != null && !CarpetServer.minecraft_server.isRunning()) return; //prevent errors when message comes while stopping
-            callHandlerInBotApps(bot,() -> {
-                return Arrays.asList(new SelectMenuInteractionValue(selectMenuInteraction));
-            }, () -> {
-                try {
-                    return CarpetServer.minecraft_server.getCommandSource().withWorld(CarpetServer.minecraft_server.getWorld(World.OVERWORLD));
-                } catch (NullPointerException npe) {
-                    return null;
-                }
-            });
+            callHandlerInBotApps(bot,
+                    () -> List.of(new SelectMenuInteractionValue(selectMenuInteraction)), 
+                    DEFAULT_SOURCE_SUPPLIER
+            );
         }
     };
 
     public void callHandlerInBotApps(Bot triggerBot, Supplier<List<Value>> argumentSupplier, Supplier<ServerCommandSource> cmdSourceSupplier) {
+        if(CarpetServer.minecraft_server != null && !CarpetServer.minecraft_server.isRunning()) return; //prevent errors when event triggers while stopping server
         ((CallbackListAccessor) handler).callRemoveCallsIf(callback -> !triggerBot.equals(Discarpet.getBotInHost(CarpetServer.scriptServer.getAppHostByName(callback.host))));
         handler.call(argumentSupplier,cmdSourceSupplier);
     }
