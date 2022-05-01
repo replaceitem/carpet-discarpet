0c2e5616a71d4586c0c3cacff95f5ee8b8ef94b9
replaceitem <40722305+replaceitem@users.noreply.github.com>
2022-05-01 21:36:54 +0200

    Made scarpet graphics images work as image fields in parsables

--- src/main/java/Discarpet/script/parsable/parsables/EmbedFooterParsable.java
+++ src/main/java/Discarpet/script/parsable/parsables/EmbedFooterParsable.java
@@ -1,18 +1,35 @@
 package Discarpet.script.parsable.parsables;
 
+import Discarpet.Discarpet;
 import Discarpet.script.parsable.Applicable;
 import Discarpet.script.parsable.Optional;
 import Discarpet.script.parsable.ParsableClass;
+import Discarpet.script.util.ScarpetGraphicsDependency;
+import carpet.script.value.Value;
 import org.javacord.api.entity.message.embed.EmbedBuilder;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
 
 @ParsableClass(name = "embed_footer")
 public class EmbedFooterParsable implements Applicable<EmbedBuilder> {
 
     String text;
-    @Optional String icon;
+    @Optional Value icon;
     
     @Override
     public void apply(EmbedBuilder embedBuilder) {
-        embedBuilder.setFooter(text,icon);
+        if(Discarpet.isScarpetGraphicsInstalled() && ScarpetGraphicsDependency.isPixelAccessible(icon)) {
+            BufferedImage image = ScarpetGraphicsDependency.getImageFromValue(icon);
+            embedBuilder.setFooter(text,image);
+            return;
+        }
+        String iconString = icon.getString();
+        File file = new File(iconString);
+        if(file.exists()) {
+            embedBuilder.setFooter(text, file);
+            return;
+        }
+        embedBuilder.setFooter(text, iconString);
     }
 }
--- src/main/java/Discarpet/script/parsable/parsables/WebhookProfileUpdaterParsable.java
+++ src/main/java/Discarpet/script/parsable/parsables/WebhookProfileUpdaterParsable.java
@@ -1,11 +1,16 @@
 package Discarpet.script.parsable.parsables;
 
+import Discarpet.Discarpet;
 import Discarpet.script.parsable.Applicable;
 import Discarpet.script.parsable.Optional;
 import Discarpet.script.parsable.ParsableClass;
+import Discarpet.script.util.ScarpetGraphicsDependency;
 import carpet.script.exception.InternalExpressionException;
+import carpet.script.value.Value;
 import org.javacord.api.entity.webhook.WebhookUpdater;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
 
@@ -13,17 +18,32 @@ import java.net.URL;
 public class WebhookProfileUpdaterParsable implements Applicable<WebhookUpdater> {
     
     @Optional String name;
-    @Optional String avatar;
+    @Optional Value avatar;
     @Optional String reason;
 
     @Override
     public void apply(WebhookUpdater webhookUpdater) {
         webhookUpdater.setName(name);
-        try {
-            webhookUpdater.setAvatar(new URL(avatar));
-        } catch (MalformedURLException e) {
-            throw new InternalExpressionException("Invalid avatar URL: " + e);
-        }
+        setAvatar(webhookUpdater);
         webhookUpdater.setAuditLogReason(reason);
+    }
+
+    private void setAvatar(WebhookUpdater webhookUpdater) {
+        if(Discarpet.isScarpetGraphicsInstalled() && ScarpetGraphicsDependency.isPixelAccessible(avatar)) {
+            BufferedImage bufferedImage = ScarpetGraphicsDependency.getImageFromValue(avatar);
+            webhookUpdater.setAvatar(bufferedImage);
+            return;
+        }
+        String iconString = avatar.getString();
+        File file = new File(iconString);
+        if(file.exists()) {
+            webhookUpdater.setAvatar(file);
+            return;
+        }
+        try {
+            webhookUpdater.setAvatar(new URL(iconString));
+        } catch (MalformedURLException e) {
+            throw new InternalExpressionException("Invalid URL/File provided for 'avatar'");
+        }
     }
 }
