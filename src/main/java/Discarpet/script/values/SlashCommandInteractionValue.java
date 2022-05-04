84ffdcfb18a41d41bd7b52070e5239744b90d15b
replaceitem <40722305+replaceitem@users.noreply.github.com>
2022-05-04 19:21:28 +0200

    Added arguments_by_name

--- src/main/java/Discarpet/script/values/SlashCommandInteractionValue.java
+++ src/main/java/Discarpet/script/values/SlashCommandInteractionValue.java
@@ -2,9 +2,15 @@ package Discarpet.script.values;
 
 import Discarpet.script.values.common.InteractionValue;
 import carpet.script.value.ListValue;
+import carpet.script.value.MapValue;
 import carpet.script.value.StringValue;
 import carpet.script.value.Value;
 import org.javacord.api.interaction.SlashCommandInteraction;
+import org.javacord.api.interaction.SlashCommandInteractionOption;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class SlashCommandInteractionValue extends InteractionValue<SlashCommandInteraction> {
     public SlashCommandInteractionValue(SlashCommandInteraction slashCommandInteraction) {
@@ -19,7 +25,21 @@ public class SlashCommandInteractionValue extends InteractionValue<SlashCommandI
             case "user" -> UserValue.of(value.getUser());
             case "token" -> StringValue.of(value.getToken());
             case "arguments" -> ListValue.wrap((value.getArguments().stream().map(SlashCommandInteractionOptionValue::new)));
+            case "arguments_by_name" -> getArgumentsByName();
             default -> Value.NULL;
         };
+    }
+
+    private Value getArgumentsByName() {
+        final Map<Value, Value> optionsMap = new HashMap<>();
+        this.addOptions(this.value.getArguments(), optionsMap);
+        return MapValue.wrap(optionsMap);
+    }
+    
+    private void addOptions(List<SlashCommandInteractionOption> options, final Map<Value, Value> map) {
+        for (SlashCommandInteractionOption slashCommandInteractionOption : options) {
+            map.put(StringValue.of(slashCommandInteractionOption.getName()), SlashCommandInteractionOptionValue.of(slashCommandInteractionOption));
+            this.addOptions(slashCommandInteractionOption.getOptions(),map);
+        }
     }
 }
