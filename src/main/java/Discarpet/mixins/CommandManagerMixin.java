0abda4fec4e89e2d2d8786e80dba10ff90bdcbd2
replaceitem <40722305+replaceitem@users.noreply.github.com>
2022-07-29 14:35:25 +0200

    Updating chat events

--- src/main/java/Discarpet/mixins/CommandManagerMixin.java
+++ src/main/java/Discarpet/mixins/CommandManagerMixin.java
@@ -1,6 +1,7 @@
 package Discarpet.mixins;
 
 import Discarpet.script.events.MiscEvents;
+import com.mojang.brigadier.ParseResults;
 import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.ServerCommandSource;
 import org.spongepowered.asm.mixin.Mixin;
@@ -11,7 +12,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(CommandManager.class)
 public class CommandManagerMixin {
     @Inject(method = "execute", at = @At("HEAD"))
-    private void onCommandExecuted(ServerCommandSource commandSource, String command, CallbackInfoReturnable<Integer> cir) {
-        MiscEvents.COMMAND_EXECUTED.onCommandExecuted(commandSource.getPlayer(), command);
+    private void onCommandExecuted(ParseResults<ServerCommandSource> parseResults, String command, CallbackInfoReturnable<Integer> cir) {
+        MiscEvents.COMMAND_EXECUTED.onCommandExecuted(parseResults.getContext().getSource().getPlayer(), command);
     }
 }
--- src/main/java/Discarpet/mixins/MinecraftServerMixin.java
+++ src/main/java/Discarpet/mixins/MinecraftServerMixin.java
@@ -1,12 +1,12 @@
 package Discarpet.mixins;
 
 import Discarpet.script.events.MiscEvents;
-import net.minecraft.network.message.MessageSender;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(MinecraftServer.class)
@@ -16,8 +16,9 @@ public abstract class MinecraftServerMixin {
 		MiscEvents.SYSTEM_MESSAGE.onSystemMessage(message);
 	}
 	
-	@Inject(at = @At("RETURN"),method = "logChatMessage")
-	public void onChatMessage(MessageSender sender, Text message, CallbackInfo ci) {
-		MiscEvents.SYSTEM_MESSAGE.onSystemMessage(Text.translatable("chat.type.text", sender.name(), message));
+	@Redirect(at = @At(value = "INVOKE", target = "Lnet/minecraft/text/Text;getString()Ljava/lang/String;"),method = "logChatMessage")
+	public String onChatMessage(Text text) {
+		MiscEvents.SYSTEM_MESSAGE.onSystemMessage(text);
+		return text.getString();
 	}
 }
