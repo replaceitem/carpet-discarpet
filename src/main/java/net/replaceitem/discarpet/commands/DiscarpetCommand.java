e9d51403f57e9bb894ca581f259ecd2388268ae4
replaceitem <40722305+replaceitem@users.noreply.github.com>
2023-06-01 22:43:52 +0200

    Updated to MC 1.20

--- src/main/java/net/replaceitem/discarpet/commands/DiscarpetCommand.java
+++ src/main/java/net/replaceitem/discarpet/commands/DiscarpetCommand.java
@@ -26,39 +26,44 @@ public class DiscarpetCommand {
     public static void register(CommandDispatcher<ServerCommandSource> dispatcher)
     {
         dispatcher.register(literal("discarpet").requires(serverCommandSource -> serverCommandSource.hasPermissionLevel(2)).executes(commandContext->{
-            String version = FabricLoader.getInstance().getModContainer("discarpet").get().getMetadata().getVersion().getFriendlyString();
-            MutableText text = Text.literal("Discarpet version " + version).formatted(Formatting.BLUE);
-            text.append("\nFor help, see the ");
-            text.append(Text.literal("documentation").setStyle(Style.EMPTY
-                    .withClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL,"https://github.com/replaceitem/carpet-discarpet/blob/master/docs/Full.md"))
-                    .withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT,Text.literal("Click to get to the Discarpet documentation")))
-                    .withFormatting(Formatting.UNDERLINE)
-                    .withColor(Formatting.DARK_BLUE)));
-            commandContext.getSource().sendFeedback(text,true);
+            commandContext.getSource().sendFeedback(() -> {
+                String version = FabricLoader.getInstance().getModContainer("discarpet").get().getMetadata().getVersion().getFriendlyString();
+                MutableText text = Text.literal("Discarpet version " + version).formatted(Formatting.BLUE);
+                text.append("\nFor help, see the ");
+                text.append(Text.literal("documentation").setStyle(Style.EMPTY
+                        .withClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL,"https://github.com/replaceitem/carpet-discarpet/blob/master/docs/Full.md"))
+                        .withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT,Text.literal("Click to get to the Discarpet documentation")))
+                        .withFormatting(Formatting.UNDERLINE)
+                        .withColor(Formatting.DARK_BLUE)));
+                return text;
+            },true);
             return 1;
         }).then(literal("list").
             executes(commandContext->{
-                Set<String> botIDs = Discarpet.discordBots.keySet();
-                final MutableText t;
-                if(botIDs.size()==0) {
-                    t = Text.literal("There are no bots active:\n").formatted(Formatting.RED);
-                } else {
-                    t = Text.literal("There are " + botIDs.size() + " bots active\n").formatted(Formatting.GREEN);
-                }
-                botIDs.forEach(id-> t.append(Text.literal(id + "\n").formatted(Formatting.BLUE)));
-                commandContext.getSource().sendFeedback(t,true);
-                return botIDs.size();
+                commandContext.getSource().sendFeedback(() -> {
+                    Set<String> botIDs = Discarpet.discordBots.keySet();
+                    final MutableText t;
+                    if(botIDs.size()==0) {
+                        t = Text.literal("There are no bots active:\n").formatted(Formatting.RED);
+                    } else {
+                        t = Text.literal("There are " + botIDs.size() + " bots active\n").formatted(Formatting.GREEN);
+                    }
+                    botIDs.forEach(id-> t.append(Text.literal(id + "\n").formatted(Formatting.BLUE)));
+                    return t;
+                },true);
+                return Discarpet.discordBots.size();
             })
         ).then(literal("getInvite").then(CommandManager.argument("id", StringArgumentType.string()).suggests(BOTS).executes(commandContext->{
-            String id = StringArgumentType.getString(commandContext,"id");
-            String invite = Discarpet.discordBots.get(id).getInvite();
-            Text text = Text.literal("Click here to get the invite link for the bot")
-                    .styled((style) -> style.withColor(Formatting.BLUE)
-                            .withFormatting(Formatting.UNDERLINE)
-                            .withClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, invite))
-                            .withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, Text.literal("Click to open the invite link")))
-                            .withInsertion(invite));
-            commandContext.getSource().sendFeedback(text,false);
+            commandContext.getSource().sendFeedback(() -> {
+                String id = StringArgumentType.getString(commandContext,"id");
+                String invite = Discarpet.discordBots.get(id).getInvite();
+                return Text.literal("Click here to get the invite link for the bot")
+                        .styled((style) -> style.withColor(Formatting.BLUE)
+                                .withFormatting(Formatting.UNDERLINE)
+                                .withClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, invite))
+                                .withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, Text.literal("Click to open the invite link")))
+                                .withInsertion(invite));
+            },false);
             return 1;
         }))).then(literal("reload").executes(commandContext->{
                 Discarpet.loadConfig(commandContext.getSource());
