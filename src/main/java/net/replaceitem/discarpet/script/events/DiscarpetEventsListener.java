1b7f76090b3ca86d5bc113f58a1342c7dee47a21
replaceitem <40722305+replaceitem@users.noreply.github.com>
2024-09-28 00:57:57 +0200

    Added message edit, delete, server member join/leave events

--- src/main/java/net/replaceitem/discarpet/script/events/DiscarpetEventsListener.java
+++ src/main/java/net/replaceitem/discarpet/script/events/DiscarpetEventsListener.java
@@ -5,27 +5,41 @@ import net.minecraft.server.ServerTask;
 import net.replaceitem.discarpet.config.Bot;
 import org.javacord.api.entity.message.Message;
 import org.javacord.api.entity.message.Reaction;
+import org.javacord.api.entity.server.Server;
+import org.javacord.api.entity.user.User;
 import org.javacord.api.event.interaction.*;
 import org.javacord.api.event.message.MessageCreateEvent;
+import org.javacord.api.event.message.MessageDeleteEvent;
+import org.javacord.api.event.message.MessageEditEvent;
 import org.javacord.api.event.message.reaction.ReactionAddEvent;
 import org.javacord.api.event.message.reaction.ReactionRemoveEvent;
+import org.javacord.api.event.server.member.ServerMemberJoinEvent;
+import org.javacord.api.event.server.member.ServerMemberLeaveEvent;
 import org.javacord.api.interaction.*;
 import org.javacord.api.listener.interaction.*;
 import org.javacord.api.listener.message.MessageCreateListener;
+import org.javacord.api.listener.message.MessageDeleteListener;
+import org.javacord.api.listener.message.MessageEditListener;
 import org.javacord.api.listener.message.reaction.ReactionAddListener;
 import org.javacord.api.listener.message.reaction.ReactionRemoveListener;
+import org.javacord.api.listener.server.member.ServerMemberJoinListener;
+import org.javacord.api.listener.server.member.ServerMemberLeaveListener;
 
 import java.util.Optional;
 
 public class DiscarpetEventsListener implements
         MessageCreateListener,
+        MessageEditListener,
+        MessageDeleteListener,
         ReactionAddListener,
         ReactionRemoveListener,
         SlashCommandCreateListener,
         MessageComponentCreateListener,
         ModalSubmitListener,
         MessageContextMenuCommandListener,
-        UserContextMenuCommandListener
+        UserContextMenuCommandListener,
+        ServerMemberJoinListener,
+        ServerMemberLeaveListener
 {
     
     protected final Bot bot;
@@ -38,6 +52,20 @@ public class DiscarpetEventsListener implements
     public void onMessageCreate(MessageCreateEvent event) {
         final Message message = event.getMessage();
         callEventOnGameThread(() -> DiscordEvents.DISCORD_MESSAGE.run(bot,message));
+    }
+
+    @Override
+    public void onMessageEdit(MessageEditEvent event) {
+        final Message message = event.getMessage();
+        final Optional<Message> oldMessage = event.getOldMessage();
+        final boolean isActualEdit = event.isActualEdit();
+        callEventOnGameThread(() -> DiscordEvents.DISCORD_MESSAGE_EDIT.run(bot,message, oldMessage, isActualEdit));
+    }
+
+    @Override
+    public void onMessageDelete(MessageDeleteEvent event) {
+        final Optional<Message> message = event.getMessage();
+        callEventOnGameThread(() -> DiscordEvents.DISCORD_MESSAGE_DELETE.run(bot,message));
     }
 
     @Override
@@ -95,6 +123,20 @@ public class DiscarpetEventsListener implements
     public void onUserContextMenuCommand(UserContextMenuCommandEvent event) {
         final UserContextMenuInteraction userContextMenuInteraction = event.getUserContextMenuInteraction();
         callEventOnGameThread(() -> DiscordEvents.DISCORD_USER_CONTEXT_MENU.run(bot, userContextMenuInteraction));
+    }
+
+    @Override
+    public void onServerMemberJoin(ServerMemberJoinEvent event) {
+        final Server server = event.getServer();
+        final User user = event.getUser();
+        callEventOnGameThread(() -> DiscordEvents.DISCORD_SERVER_MEMBER_JOIN.run(bot, server, user));
+    }
+
+    @Override
+    public void onServerMemberLeave(ServerMemberLeaveEvent event) {
+        final Server server = event.getServer();
+        final User user = event.getUser();
+        callEventOnGameThread(() -> DiscordEvents.DISCORD_SERVER_MEMBER_LEAVE.run(bot, server, user));
     }
 
     private static void callEventOnGameThread(Runnable runnable) {
