7b730868847f12211f92cbbfc2f5b9b04d9d61f3
replaceitem <40722305+replaceitem@users.noreply.github.com>
2025-05-01 12:32:24 +0200

    Added config option for member caching policy

--- src/main/java/net/replaceitem/discarpet/Discarpet.java
+++ src/main/java/net/replaceitem/discarpet/Discarpet.java
@@ -10,6 +10,7 @@ import carpet.script.value.StringValue;
 import carpet.script.value.Value;
 import com.mojang.brigadier.CommandDispatcher;
 import net.dv8tion.jda.api.requests.GatewayIntent;
+import net.dv8tion.jda.api.utils.MemberCachePolicy;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.command.CommandRegistryAccess;
@@ -33,10 +34,7 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Objects;
-import java.util.Set;
+import java.util.*;
 import java.util.concurrent.CompletableFuture;
 import java.util.stream.Collectors;
 
@@ -44,7 +42,7 @@ public class Discarpet implements CarpetExtension, ModInitializer {
 	public static final Logger LOGGER = LogManager.getLogger("Discarpet");
 	protected static ConfigManager configManager;
 
-	public static Map<String, Bot> discordBots = new HashMap<>();
+	public static Map<String, Bot> discordBots = Collections.synchronizedMap(new HashMap<>());
 
 	@Override
 	public void onInitialize() {
@@ -106,33 +104,50 @@ public class Discarpet implements CarpetExtension, ModInitializer {
 
 		for (BotConfig botConfig : configManager.getConfig().BOTS) {
 			if(botConfig == null) continue;
-			// TODO migrate intent names?
-			Set<GatewayIntent> intents = botConfig.INTENTS.stream()
-					.map(s -> ValueUtil.getEnum(GatewayIntent.class, s).orElse(null))
-					.filter(Objects::nonNull)
-					.collect(Collectors.toSet());
-
 			String botId = botConfig.BOT_ID;
+            try {
+                Set<GatewayIntent> intents = botConfig.INTENTS.stream()
+                        .map(s -> ValueUtil.getEnum(GatewayIntent.class, s)
+								.orElseThrow(() -> new IllegalArgumentException("Unknown intent: " + s))
+						)
+                        .filter(Objects::nonNull)
+                        .collect(Collectors.toSet());
+				
+				MemberCachePolicy memberCachePolicy = switch (botConfig.MEMBER_CACHE_POLICY.toLowerCase()) {
+					case "all" -> MemberCachePolicy.ALL;
+					case "online" -> MemberCachePolicy.ONLINE;
+					case "voice" -> MemberCachePolicy.VOICE;
+					case "pending" -> MemberCachePolicy.PENDING;
+					case "booster" -> MemberCachePolicy.BOOSTER;
+					case "owner" -> MemberCachePolicy.OWNER;
+					case "none" -> MemberCachePolicy.NONE;
+                    default -> throw new IllegalArgumentException("Unknown member cache policy: " + botConfig.MEMBER_CACHE_POLICY);
+                };
 
-			CompletableFuture<Bot> botCompletableFuture = Bot.create(botId, botConfig.BOT_TOKEN, intents);
-			botCompletableFuture.exceptionally(throwable -> {
-				String error = "Could not login bot " + botId;
-				LOGGER.warn(error,throwable);
-				if(source != null) source.sendFeedback(() -> Text.literal(error).formatted(Formatting.RED),false);
-				return null;
-			});
-			botCompletableFuture.thenAccept(bot -> {
-				if(bot == null) return;
-				discordBots.put(bot.getId(), bot);
-				String msg = "Bot " + botId + " sucessfully logged in";
-				if(!intents.isEmpty()) {
-					msg += " with intents " + intents.stream().map(Enum::toString).collect(Collectors.joining(","));
-				}
-				MutableText text = Text.literal(msg).styled(style -> style.withColor(Formatting.GREEN));
-				if(source != null) source.sendFeedback(() -> text,false);
-				LOGGER.info(msg);
-			});
-		}
+                CompletableFuture<Bot> botCompletableFuture = Bot.create(botId, botConfig.BOT_TOKEN, intents, memberCachePolicy);
+                botCompletableFuture.exceptionally(throwable -> {
+                    String error = "Could not login bot " + botId;
+                    LOGGER.warn(error, throwable);
+                    if(source != null) source.sendFeedback(() -> Text.literal(error + ": " + throwable.getMessage()).formatted(Formatting.RED),false);
+                    return null;
+                });
+                botCompletableFuture.thenAccept(bot -> {
+                    if(bot == null) return;
+                    discordBots.put(bot.getId(), bot);
+                    String msg = "Bot " + botId + " sucessfully logged in";
+                    if(!intents.isEmpty()) {
+                        msg += " with intents " + intents.stream().map(Enum::toString).collect(Collectors.joining(","));
+                    }
+                    MutableText text = Text.literal(msg).styled(style -> style.withColor(Formatting.GREEN));
+                    if(source != null) source.sendFeedback(() -> text,false);
+                    LOGGER.info(msg);
+                });
+            } catch (Exception e) {
+				String error = "Could not create bot " + botId;
+				LOGGER.warn(error, e);
+				if(source != null) source.sendFeedback(() -> Text.literal(error + ": " + e.getMessage()).formatted(Formatting.RED),false);
+            }
+        }
 	}
 
 	public static boolean isScarpetGraphicsInstalled() {
