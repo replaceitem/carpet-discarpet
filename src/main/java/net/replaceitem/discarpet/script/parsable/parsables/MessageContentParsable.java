59376255783c2e8b1473ef3736eddab97bc62509
replaceitem <40722305+replaceitem@users.noreply.github.com>
2025-05-09 01:14:16 +0200

    Restructured how message content parsable is applied

--- src/main/java/net/replaceitem/discarpet/script/parsable/parsables/MessageContentParsable.java
+++ src/main/java/net/replaceitem/discarpet/script/parsable/parsables/MessageContentParsable.java
@@ -7,10 +7,13 @@ import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.sticker.Sticker;
 import net.dv8tion.jda.api.interactions.components.ActionRow;
 import net.dv8tion.jda.api.interactions.components.ItemComponent;
+import net.dv8tion.jda.api.requests.RestAction;
 import net.dv8tion.jda.api.requests.restaction.MessageCreateAction;
 import net.dv8tion.jda.api.requests.restaction.WebhookMessageCreateAction;
 import net.dv8tion.jda.api.utils.FileUpload;
-import net.dv8tion.jda.api.utils.messages.MessageRequest;
+import net.dv8tion.jda.api.utils.messages.AbstractMessageBuilder;
+import net.dv8tion.jda.api.utils.messages.MessageCreateBuilder;
+import net.dv8tion.jda.api.utils.messages.MessageData;
 import net.replaceitem.discarpet.script.parsable.DirectParsable;
 import net.replaceitem.discarpet.script.parsable.OptionalField;
 import net.replaceitem.discarpet.script.parsable.ParsableClass;
@@ -50,50 +53,79 @@ public class MessageContentParsable implements DirectParsable {
     Boolean suppress_embeds = false;
     @OptionalField
     Boolean suppress_notifications = false;
-    
-    @SuppressWarnings("ResultOfMethodCallIgnored")
-    public void apply(MessageRequest<?> message) {
-        message.setContent(content);
 
-        message.setFiles(Stream.of(
+    /**
+     * The fields of message content are spread through the three class hierachies of the
+     * {@link AbstractMessageBuilder}, {@link MessageData} and a message rest action like {@link MessageCreateAction}.
+     * Some fields only exist in the later rest action object,
+     * while some need to already be set in the builder (like content).
+     * That is why the functions for converting between these phases need to be passed,
+     * in order to apply the parsable's data all in one place.
+     */
+    @SuppressWarnings("ResultOfMethodCallIgnored")
+    public <B extends AbstractMessageBuilder<D,B>,D extends MessageData,A extends RestAction<?>> A apply(B builder, Function<B,D> build, Function<D,A> send) {
+        builder.setContent(content);
+
+        builder.setFiles(Stream.of(
                 attachments.stream(),
                 embeds.stream().flatMap(embedParsable -> embedParsable.getFileUploads().stream())
         ).flatMap(Function.identity()).toList());
         
-        if(!stickers.isEmpty()) {
-            if(!(message instanceof MessageCreateAction messageCreateAction)) throw new InternalExpressionException("'stickers' is only supported for message creation, not edits");
-            messageCreateAction.setStickers(stickers.stream().map(value ->
-                    Sticker.fromId(value instanceof StickerValue stickerValue ?
-                            stickerValue.getDelegate().getId() :
-                            value.getString()
-                    )).toList()
-            );
-        }
-        
-        message.setEmbeds(embeds.stream().map(EmbedParsable::construct).toList());
-        
-        message.setComponents(components.stream().map(ActionRow::of).toList());
 
-        if(allowed_mentions != null) allowed_mentions.apply(message);
+        builder.setEmbeds(embeds.stream().map(EmbedParsable::construct).toList());
+
+        builder.setComponents(components.stream().map(ActionRow::of).toList());
+
+        if(allowed_mentions != null) allowed_mentions.apply(builder);
+
+        builder.setSuppressEmbeds(suppress_embeds);
         
-        if(message instanceof MessageCreateAction messageCreateAction) {
-            if(reply_to != null) {
+        
+        if(tts) {
+            if(builder instanceof MessageCreateBuilder messageCreateBuilder) {
+                messageCreateBuilder.setTTS(true);
+            } else throw onlyCreate("tts");
+        }
+
+        D data = build.apply(builder);
+
+        
+
+        A action = send.apply(data);
+
+        if(reply_to != null) {
+            if(action instanceof MessageCreateAction messageCreateAction) {
                 messageCreateAction.setMessageReference(reply_to);
-            }
-            if(nonce != null) {
+            } else throw onlyCreate("reply_to");
+        }
+        if(nonce != null) {
+            if(action instanceof MessageCreateAction messageCreateAction) {
                 messageCreateAction.setNonce(nonce);
-            }
-            if(tts != null) {
-                messageCreateAction.setTTS(tts);
-            }
-            messageCreateAction.setSuppressedNotifications(suppress_notifications);
+            } else throw onlyCreate("nonce");
+        }
+        if(suppress_notifications) {
+            if(action instanceof MessageCreateAction messageCreateAction) {
+                messageCreateAction.setSuppressedNotifications(suppress_notifications);
+            } else throw onlyCreate("suppress_notifications");
+
+        }
+        if(!stickers.isEmpty()) {
+            if (action instanceof MessageCreateAction messageCreateAction) {
+                messageCreateAction.setStickers(stickers.stream().map(value ->
+                        Sticker.fromId(value instanceof StickerValue stickerValue ?
+                                stickerValue.getDelegate().getId() :
+                                value.getString()
+                        )).toList()
+                );
+            } else throw onlyCreate("stickers");
+        }
+        if(ephemeral) {
+            if(action instanceof WebhookMessageCreateAction<?> webhookMessageCreateAction) {
+                webhookMessageCreateAction.setEphemeral(true);
+            } else throw new InternalExpressionException("'ephemeral' is only supported for interaction replies");
         }
         
-        message.setSuppressEmbeds(suppress_embeds);
-        
-        if(message instanceof WebhookMessageCreateAction<?> webhookMessageCreateAction) {
-            webhookMessageCreateAction.setEphemeral(ephemeral);
-        }
+        return action;
     }
 
     @Override
@@ -103,5 +135,9 @@ public class MessageContentParsable implements DirectParsable {
             return true;
         }
         return false;
+    }
+    
+    private static InternalExpressionException onlyCreate(String field) {
+        return new InternalExpressionException("'" + field + "' is only supported for message creation, not edits");
     }
 }
