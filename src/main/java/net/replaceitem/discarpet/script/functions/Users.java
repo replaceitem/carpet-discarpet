46c2f30365c45615d770d0fc70b646c4f34b880f
replaceitem <40722305+replaceitem@users.noreply.github.com>
2025-05-10 21:28:45 +0200

    Removed functions made obsolete by member value queries

--- src/main/java/net/replaceitem/discarpet/script/functions/Users.java
+++ src/main/java/net/replaceitem/discarpet/script/functions/Users.java
@@ -1,29 +1,18 @@
 package net.replaceitem.discarpet.script.functions;
 
 import carpet.script.annotation.ScarpetFunction;
-import carpet.script.value.ListValue;
-import carpet.script.value.Value;
 import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.Role;
 import net.dv8tion.jda.api.entities.User;
 import net.replaceitem.discarpet.script.exception.DiscordThrowables;
 import net.replaceitem.discarpet.script.util.ValueUtil;
-import net.replaceitem.discarpet.script.values.RoleValue;
 
-import java.awt.*;
 import java.time.Instant;
 import java.util.Optional;
 
 @SuppressWarnings({"unused", "OptionalUsedAsFieldOrParameterType"})
 public class Users {
-    @ScarpetFunction
-    public String dc_get_display_name(User user, Guild server) {
-        Member member = server.getMember(user);
-        if(member == null) return null;
-        return member.getEffectiveName();
-    }
-
     @ScarpetFunction(maxParams = 4)
     public void dc_set_nickname(User user, Guild server, String name, String... reason) {
         Member member = server.getMember(user);
@@ -43,28 +32,6 @@ public class Users {
         Member member = role.getGuild().getMember(user);
         if(member == null) throw DiscordThrowables.genericMessage("Unknown member");
         ValueUtil.awaitRest(role.getGuild().removeRoleFromMember(user, role).reason(ValueUtil.optionalArg(reason)), "Could not remove role to user");
-    }
-
-    @ScarpetFunction
-    public Value dc_get_user_roles(User user, Guild server) {
-        Member member = server.getMember(user);
-        if(member == null) return Value.NULL;
-        return ListValue.wrap(member.getRoles().stream().map(RoleValue::of));
-    }
-
-    @ScarpetFunction
-    public Value dc_get_user_color(User user, Guild server) {
-        Member member = server.getMember(user);
-        if(member == null) return Value.NULL;
-        Color color = member.getColor();
-        return color == null ? Value.NULL : ValueUtil.colorToValue(color);
-    }
-
-    @ScarpetFunction
-    public Value dc_get_timeout(User user, Guild server) {
-        Member member = server.getMember(user);
-        if(member == null) return Value.NULL;
-        return ValueUtil.ofTime(member.getTimeOutEnd());
     }
     
     @ScarpetFunction(maxParams = 4)
